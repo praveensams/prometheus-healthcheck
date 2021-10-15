a871d215e8da13388b67e7cb131660a54c0f2b63
praveensams16 <praveensams16@gmail.com>
2021-10-15 19:15:58 +0400

    Update health.go

--- health.go
+++ health.go
@@ -1,55 +1,77 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "bufio"
-    "bytes"
-    "regexp"
-    "log"
-    "net/http"
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 )
 
-func filter( url string , filter string, c chan string ) {
-    response, _ := http.Get(url)
-    buf := new(bytes.Buffer)
-    buf.ReadFrom(response.Body)
-    newStr := buf.String()
-    re := regexp.MustCompile(filter)
-    list := re.FindAllString(newStr,-1)
-    if len(list) > 0 {
-        c <- "sam_health{instance=\"" + url + "\"}" + "  1\n"
-    } else {
-        c <- "sam_health{instance=\"" + url + "\"}" + "  0\n"
-    }
-  }
+//var wg sync.WaitGroup
 
-  func handleRequests() {
-    http.HandleFunc("/metrics", homePage)
-    log.Fatal(http.ListenAndServe(":9101", nil))
+func filter(url string, filter string, c chan string) {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	response, err := client.Get(url)
+
+	if err != nil {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println("panic occurred:", err)
+				c <- "app_health{instance=\"" + url + "\"}" + "  0"
+			}
+		}()
+		response.Body.Close()
+
+	} else {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(response.Body)
+		newStr := strings.ToLower(buf.String())
+		re := regexp.MustCompile(filter)
+		list := re.FindAllString(newStr, -1)
+		if len(list) > 0 {
+			c <- "app_health{instance=\"" + url + "\"}" + "  1"
+		} else {
+			c <- "app_health{instance=\"" + url + "\"}" + "  0"
+		}
+		response.Body.Close()
+
+	}
 }
 
+func handleRequests() {
+	http.HandleFunc("/metrics", homePage)
+	log.Fatal(http.ListenAndServe(":9101", nil))
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    c := make(chan string,2)
-    file, err := os.Open("./url.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	c := make(chan string)
+	file, err := os.Open("./url.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        go filter(scanner.Text(),`lx=function(){}};google`,c)
-        fmt.Fprintf(w,<-c)
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+		go filter(scanner.Text(), `head><meta content`, c)
+		fmt.Fprintf(w, <-c+"\n")
+	}
 
-  }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
 
 func main() {
-    handleRequests()
+	handleRequests()
 }
